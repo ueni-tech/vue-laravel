c392b06c6fa44d950235366edc219306fbecbabb
Kosuke Uenishi <uenishi_kosuke@ca-adv.co.jp>
2023-12-25 17:13:12 +0900

    #1 vue-router, vuetify: app.jsの設定

--- laravel_vite-example/resources/js/app.js
+++ laravel_vite-example/resources/js/app.js
@@ -2,4 +2,12 @@ import './bootstrap';
 import '../css/app.css';
 import { createApp } from 'vue';
 import App from './App.vue';
-createApp(App).mount("#app");
+import { createVuetify } from 'vuetify';
+import router from './router';
+import 'vuetify/dist/vuetify.min.css';
+
+const app = createApp(App);
+const vuetify = createVuetify();
+app.use(router);
+app.use(vuetify);
+app.mount('#app');
