2acf12b6793830542004feb233028f474c45fad2
Kosuke Uenishi <uenishi_kosuke@ca-adv.co.jp>
2023-12-25 17:24:44 +0900

    #1 web.phpを修正

--- laravel_vite-example/routes/web.php
+++ laravel_vite-example/routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// localhost:8000以下のURLが何であってもapp.blade.phpを返すようする
+Route::get('/{any?}', fn () => view('app'))->where('any', '.+');
